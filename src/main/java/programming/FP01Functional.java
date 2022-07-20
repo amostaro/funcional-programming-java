178e58647d72713b9674b8fe222f1c2182622649
amostaro <amostaro@ciandt.com>
2022-07-20 16:36:37 -0300

    functional MAP

--- src/main/java/programming/FP01Functional.java
+++ src/main/java/programming/FP01Functional.java
@@ -10,6 +10,8 @@ public class FP01Functional {
         printAllNumbersInListFunctional(numbers);
         printEvenNumbersInListFunctional(numbers);
         printOddNumbersInListFunctional(numbers);
+        printSquaresOfEvenNumbersInListFunctional(numbers);
+        printSquaresOfOddNumbersInListFunctional(numbers);
     }
 
 //    private static void print(int number) {
@@ -50,6 +52,32 @@ public class FP01Functional {
         System.out.println("Functional Odd Numbers");
         numbers.stream()
                 .filter(number -> number%2 == 1) // filter - lambda expression
+                .forEach(System.out :: println); // method reference
+
+    }
+
+    private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+
+        // [12, 9, 13, 4, 6, 2, 4, 12, 15] - na vertical
+        System.out.println("Functional Squares Of Even Numbers");
+        numbers.stream()
+//                .filter(FP01Functional :: isEven) // filter - method reference
+                .filter(number -> number%2 == 0) // filter - lambda expression
+                // mapping x -> x * x
+                .map(number -> number * number)
+                .forEach(System.out :: println); // method reference
+
+    }
+
+    private static void printSquaresOfOddNumbersInListFunctional(List<Integer> numbers) {
+
+        // [12, 9, 13, 4, 6, 2, 4, 12, 15] - na vertical
+        System.out.println("Functional Squares Of Odd Numbers");
+        numbers.stream()
+//                .filter(FP01Functional :: isEven) // filter - method reference
+                .filter(number -> number%2 == 1) // filter - lambda expression
+                // mapping x -> x * x
+                .map(number -> number * number)
                 .forEach(System.out :: println); // method reference
 
     }
