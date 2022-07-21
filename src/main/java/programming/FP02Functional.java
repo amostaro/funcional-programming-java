994406decb70230650b4208c4d5692698f3029ad
amostaro <amostaro@ciandt.com>
2022-07-21 08:37:27 -0300

    operation stream reduce

---
+++ src/main/java/programming/FP02Functional.java
@@ -0,0 +1,25 @@
+package programming;
+
+import java.util.List;
+
+public class FP02Functional {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+        int sum = addListStructured(numbers);
+        System.out.println(sum);
+
+    }
+
+    private static int addListStructured(List<Integer> numbers) {
+
+        return numbers.stream()
+                .reduce(0, FP02Functional :: sum);
+
+    }
+
+    private static Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+}
