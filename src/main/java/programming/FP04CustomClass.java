dfbc37fcb5e5267138b968651367acea2de491e2
amostaro <amostaro@ciandt.com>
2022-07-21 16:54:52 -0300

    max, min, orElse, findFirst, findAny

--- src/main/java/programming/FP04CustomClass.java
+++ src/main/java/programming/FP04CustomClass.java
@@ -72,14 +72,14 @@ public class FP04CustomClass {
                 );
 
         // allMatch, noneMatch, anyMatch - return TRUE or FALSE
-//        Predicate<Course> reviewScoreGreaterThan95Predicate = course -> course.getReviewScore() > 95;
-//        System.out.println(coursesList.stream().allMatch(reviewScoreGreaterThan95Predicate));
-//
-//        Predicate<Course> reviewScoreGreaterThan90Predicate = course -> course.getReviewScore() > 90;
-//        System.out.println(coursesList.stream().noneMatch(reviewScoreGreaterThan90Predicate));
-//
-//        Predicate<Course> reviewScoreLessThan90Predicate = course -> course.getReviewScore() < 90;
-//        System.out.println(coursesList.stream().anyMatch(reviewScoreLessThan90Predicate));
+        Predicate<Course> reviewScoreGreaterThan95Predicate = course -> course.getReviewScore() > 95;
+        System.out.println(coursesList.stream().allMatch(reviewScoreGreaterThan95Predicate));
+
+        Predicate<Course> reviewScoreGreaterThan90Predicate = course -> course.getReviewScore() > 90;
+        System.out.println(coursesList.stream().noneMatch(reviewScoreGreaterThan90Predicate));
+
+        Predicate<Course> reviewScoreLessThan90Predicate = course -> course.getReviewScore() < 90;
+        System.out.println(coursesList.stream().anyMatch(reviewScoreLessThan90Predicate));
 
         Comparator<Course> comparingByNoOfStudents = Comparator.comparingInt(Course::getNoOfStudents);
         System.out.println(coursesList.stream()
@@ -135,6 +135,30 @@ public class FP04CustomClass {
                 .collect(Collectors.toList()));
         // [Fullstack 14000 91, AWS 21000 92, Azure 21000 99, Docker 20000 92, Kubernetes 20000 91]
 
-
+        Comparator<Course> comparingByNoOfStudentsAndReviewScoreReversed = Comparator.comparingInt(Course::getNoOfStudents).thenComparing(Course::getReviewScore).reversed();
+        // [Microservices 25000 96, API 22000 97, Azure 21000 99, AWS 21000 92, Spring 20000 98, Docker 20000 92, Kubernetes 20000 91, Spring Boot 18000 95, Fullstack 14000 91]
+        System.out.println(coursesList.stream()
+                .max(comparingByNoOfStudentsAndReviewScoreReversed));
+        // Optional[Fullstack 14000 91]
+        System.out.println(coursesList.stream()
+                .min(comparingByNoOfStudentsAndReviewScoreReversed));
+        // Optional[Microservices 25000 96]
+        System.out.println(coursesList.stream()
+                .filter(reviewScoreLessThan90Predicate)
+                .min(comparingByNoOfStudentsAndReviewScoreReversed));
+        // Optional.empty
+        System.out.println(coursesList.stream()
+                .filter(reviewScoreLessThan90Predicate)
+                .min(comparingByNoOfStudentsAndReviewScoreReversed)
+                .orElse( new Course("Kubernetes", "Cloud", 91, 20000))); // caso nao atenda as condicoes .orElse
+        // Kubernetes 20000 91
+        System.out.println(coursesList.stream()
+                .filter(reviewScoreGreaterThan95Predicate)
+                .findFirst());
+        // Optional[Spring 20000 98]
+        System.out.println(coursesList.stream()
+                .filter(reviewScoreGreaterThan95Predicate)
+                .findAny());
+        // Optional[Spring 20000 98]
     }
 }
