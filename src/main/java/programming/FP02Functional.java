b2a3320dee30d8877a69c526da1aa9d975132f9f
amostaro <amostaro@ciandt.com>
2022-07-21 09:22:17 -0300

    distinct, sorted, Comparator. and Comparator. with lambda exp

--- src/main/java/programming/FP02Functional.java
+++ src/main/java/programming/FP02Functional.java
@@ -1,14 +1,26 @@
 package programming;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class FP02Functional {
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices",
+                "AWS", "PCF", "Azure", "Docker", "Kubernetes");
 
-        int sum = addListStructured(numbers);
-        System.out.println(sum);
+//        int sum = addListStructured(numbers);
+//        System.out.println(sum);
+
+//        numbers.stream().distinct().forEach(System.out::println); // não repete
+//        numbers.stream().sorted().forEach(System.out::println); // ordenados
+//        numbers.stream().distinct().sorted().forEach(System.out::println); // ordenados e não repete
+
+//        courses.stream().sorted().forEach(System.out::println);
+//        courses.stream().sorted(Comparator.naturalOrder()).forEach(System.out::println); // ordem natural
+//        courses.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println); // ordem reversa
+        courses.stream().sorted(Comparator.comparing(str -> str.length())).forEach(System.out::println); // ordem pela qtd caracteres
 
     }
 
