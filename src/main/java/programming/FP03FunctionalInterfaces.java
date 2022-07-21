f04d9d1e8d98dec8ea631db2e32896d92e56169f
amostaro <amostaro@ciandt.com>
2022-07-21 11:41:06 -0300

    Predicate, Functional, Consumer - interfaces

--- src/main/java/programming/FP03FunctionalInterfaces.java
+++ src/main/java/programming/FP03FunctionalInterfaces.java
@@ -1,11 +1,49 @@
 package programming;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FP03FunctionalInterfaces {
 
     @SuppressWarnings("unused")
     public static void main(String[] args) {
         List<Integer> numbersList = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+        Predicate<Integer> isEvenPredicate = number -> number % 2 == 0;
+        Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer number) {
+                return number % 2 == 0;
+            }
+        };
+
+        Function<Integer, Integer> squareFunction = number -> number * number;
+        Function<Integer, Integer> squareFunction2 = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer number) {
+                return number * number;
+            }
+        };
+
+        Consumer<Integer> sysoutConsumer = System.out::println;
+        Consumer<Integer> sysoutConsumer2 = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer x) {
+                System.out.println(x);
+            }
+        };
+
+
+        numbersList.stream()
+                .filter(isEvenPredicate)  // Predicate
+                .map(squareFunction)      // Function
+                .forEach(sysoutConsumer); // Consumer void
+
+        numbersList.stream()
+                .filter(isEvenPredicate2)  // Predicate
+                .map(squareFunction2)      // Function
+                .forEach(sysoutConsumer2); // Consumer void
     }
 }
